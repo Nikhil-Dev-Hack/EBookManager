04af4e46ecc324a1c353b685be6d6949150063d6
Nikhil-Dev-Hack <75464384+Nikhil-Dev-Hack@users.noreply.github.com>
2022-03-17 18:05:36 +0530

    first commit

---
+++ eBookManager.Helper/ExtensionMethods.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using eBookManager.Engine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace eBookManager.Helper
+{
+    public static class ExtensionMethods
+    {
+        public static int ToInt(this string value, int defaultInteger = 0)
+        {
+            try
+            {
+                if (int.TryParse(value, out int validInteger))
+                {
+                    //Out variables
+                    return validInteger;
+
+                }
+                else
+                {
+                    return defaultInteger;
+                }
+            }
+            catch
+            {
+
+                return defaultInteger;
+            }
+        }
+        public async static Task WriteToDataStore(this List<StorageSpace> value, string storagePath, bool appendToExistingFile = false)
+        {
+            using (FileStream fs = File.Create(storagePath))
+                await JsonSerializer.SerializeAsync(fs, value);
+        }
+        public static bool StorageSpaceExists(this List<StorageSpace> space, string nameValueToCheck, out int storageSpaceId)
+        {
+            bool exists = false;
+            storageSpaceId = 0;
+            if (space.Count() != 0)
+            {
+                int count = (from r in space where r.Name.Equals(nameValueToCheck) select r).Count();
+                if (count > 0) exists = true;
+                storageSpaceId = (from r in space select r.ID).Max() + 1;
+                
+            }
+            return exists;
+        }
+
+        public async static Task<List<StorageSpace>> ReadFromDataStore(this List<StorageSpace> value, string storagePath)
+        {
+            if (!File.Exists(storagePath))
+            {
+                var newFile = File.Create(storagePath);
+                newFile.Close();
+            }
+            using FileStream fs = File.OpenRead(storagePath);
+            if (fs.Length == 0 ) return new List<StorageSpace>();
+
+            var storageList = await JsonSerializer.DeserializeAsync<List<StorageSpace>>(fs);
+
+            return storageList;
+        }
+
+    }
+}
---
+++ eBookManager/ImportBooks.cs
@@ -0,0 +1,27 @@
+﻿using eBookManager.Engine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using static eBookManager.Helper.ExtensionMethods;
+using static System.Math;
+
+namespace eBookManager
+{
+    public partial class ImportBooks : Form
+    {
+        private string _jsonPath;
+        private List<StorageSpace> _spaces;
+        private enum _storageSpaceSelection
+        {
+            New = -9999,
+            NoSelection = -1
+        }
+        public ImportBooks()
+        {
+            InitializeComponent();
+            _jsonPath = Path.Combine(Application.StartupPath, "bookData.txt");
+        }
+    }
+}
